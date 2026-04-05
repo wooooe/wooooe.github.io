96dccbacc184ac53c01625f25562269a5c35df01
wooooe <zaooen@qq.com>
2026-04-06 00:47:38 +0800

    Site updated: 2026-04-06 00:47:38

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/04/05/firstrecord/"];function toRandomPost(){
+var posts=["2026/04/05/test/","2026/04/05/firstrecord/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
